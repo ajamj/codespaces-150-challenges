34377f4543545b6db5ce34947ab8bcc03ee67d94
ajamj <50546695+ajamj@users.noreply.github.com>
2023-02-23 14:15:20 +0000

    Soal 10

---
+++ 010.py
@@ -0,0 +1,7 @@
+"""There are 2,204 pounds in a kilogram. Ask the 
+user to enter a weight in kilograms and convert it 
+to pounds."""
+
+weight_in_kg = int(input("Input the weight in kilogram: "))
+kg_to_pound = weight_in_kg*2204
+print(weight_in_kg,"kilograms equal to",kg_to_pound,"pounds.")
